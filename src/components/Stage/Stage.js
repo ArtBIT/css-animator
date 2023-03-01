2a080502f99133fd0eff4e9398fcdcb21fbe851c
Djordje Ungar <djordje.ungar@gmail.com>
2023-03-01 14:52:57 +0100

    Toggle grid

--- src/components/Stage/Stage.js
+++ src/components/Stage/Stage.js
@@ -2,6 +2,7 @@ import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
 
 import c from "classnames";
 import CenterFocusStrongIcon from "@mui/icons-material/CenterFocusStrong";
+import Grid3x3Icon from "@mui/icons-material/Grid3x3";
 import IconButton from "@mui/material/IconButton";
 import AnimationTarget from "../AnimationTarget";
 import Flex from "../Flex";
@@ -9,6 +10,10 @@ import useStageTransforms from "../../hooks/useStageTransforms.js";
 import s from "./Stage.module.css";
 
 const Stage = ({ className, innerRef, frame, updateFrame, isPlaying }) => {
+  const [showGrid, setShowGrid] = useState(true);
+  const toggleGrid = () => {
+    setShowGrid(!showGrid);
+  };
   const { stageContainerRef, stageRef, reset } = useStageTransforms();
   return (
     <div className={c(s["root"], className)}>
@@ -17,15 +22,22 @@ const Stage = ({ className, innerRef, frame, updateFrame, isPlaying }) => {
           <IconButton onClick={() => reset()} aria-label="Reset Viewport">
             <CenterFocusStrongIcon />
           </IconButton>
+          <IconButton onClick={() => toggleGrid()} aria-label="Reset Viewport">
+            <Grid3x3Icon />
+          </IconButton>
         </Flex>
         <div
           ref={stageContainerRef}
           className={s["content-wrapper"]}
-          style={{
-            backgroundImage: `url(${
-              process.env.PUBLIC_URL + "/assets/grid.png"
-            })`,
-          }}
+          style={
+            showGrid
+              ? {
+                  backgroundImage: `url(${
+                    process.env.PUBLIC_URL + "/assets/grid.png"
+                  })`,
+                }
+              : undefined
+          }
         >
           <div className={s["content"]} ref={stageRef}>
             <AnimationTarget
